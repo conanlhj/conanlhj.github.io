f71df5b51c42eadb6142a8af3793ef0724e959dc
shiggy <happyleehan@gmail.com>
2025-03-25 18:40:54 +0000

    feat: 히트맵에 툴팁 기능 추가 및 코드 정리

--- assets/js/heatmap.js
+++ assets/js/heatmap.js
@@ -3,64 +3,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // 히트맵 렌더링 함수 정의
   const renderHeatmap = () => {
-    cal.paint({
-      itemSelector: '#posts-heatmap',
+    cal.paint(
+      {
+        itemSelector: '#posts-heatmap',
 
-      data: {
-        source: '/assets/js/data/posts-heatmap.json',
-        type: 'json',
-        x: 'date',
-        y: (d) => d['value']
-      },
+        data: {
+          source: '/assets/js/data/posts-heatmap.json',
+          type: 'json',
+          x: 'date',
+          y: (d) => d['value']
+        },
 
-      range: 3,
+        range: 3,
 
-      domain: {
-        type: 'month',
-        gutter: 8,
+        domain: {
+          type: 'month',
+          gutter: 8,
 
-        label: {
-          text: 'MMM',
-          textAlign: 'left',
-          position: 'bottom'
-        }
-      },
+          label: {
+            text: 'MMM',
+            textAlign: 'left',
+            position: 'bottom'
+          }
+        },
 
-      subDomain: {
-        type: 'ghDay',
-        radius: 3,
-        width: 11,
-        height: 11,
-        gutter: 4
-      },
+        subDomain: {
+          type: 'ghDay',
+          radius: 3,
+          width: 11,
+          height: 11,
+          gutter: 4
+        },
 
-      date: { start: new Date(new Date().setMonth(new Date().getMonth() - 2)) },
+        date: {
+          start: new Date(new Date().setMonth(new Date().getMonth() - 2))
+        },
 
-      scale: {
-        color: {
-          type: 'threshold',
-          range: [
-            getComputedStyle(document.documentElement)
+        scale: {
+          color: {
+            type: 'threshold',
+            range: [
+              getComputedStyle(document.documentElement)
+                .getPropertyValue('--heatmap-cell-1')
+                .trim(),
+              getComputedStyle(document.documentElement)
+                .getPropertyValue('--heatmap-cell-2')
+                .trim(),
+              getComputedStyle(document.documentElement)
+                .getPropertyValue('--heatmap-cell-3')
+                .trim(),
+              getComputedStyle(document.documentElement)
+                .getPropertyValue('--heatmap-cell-4')
+                .trim()
+            ],
+            interpolate: 'rgb',
+            domain: [1, 2, 3],
+            unknown: getComputedStyle(document.documentElement)
               .getPropertyValue('--heatmap-cell-1')
-              .trim(),
-            getComputedStyle(document.documentElement)
-              .getPropertyValue('--heatmap-cell-2')
-              .trim(),
-            getComputedStyle(document.documentElement)
-              .getPropertyValue('--heatmap-cell-3')
-              .trim(),
-            getComputedStyle(document.documentElement)
-              .getPropertyValue('--heatmap-cell-4')
               .trim()
-          ],
-          interpolate: 'rgb',
-          domain: [1, 2, 3],
-          unknown: getComputedStyle(document.documentElement)
-            .getPropertyValue('--heatmap-cell-1')
-            .trim()
+          }
         }
-      }
-    });
+      },
+      [
+        [
+          Tooltip,
+          {
+            text: function (date, value, dayjsDate) {
+              return (
+                dayjsDate.format('YYYY-MM-DD') +
+                ' | ' +
+                (value ? value : '0') +
+                ' Posts Uploaded.'
+              );
+            }
+          }
+        ]
+      ]
+    );
   };
 
   // 페이지 로드 시 최초 렌더링
